4d4e797950a1a10ef96f7d44c09b432c9340bb7a
Bittu Gupta <120164449+iguptabittu@users.noreply.github.com>
2025-01-26 13:55:06 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,1 +1,20 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnets" {
+  description = "The IDs of the public subnets"
+  value       = module.vpc.public_subnets
+}
+
+output "private_subnets" {
+  description = "The IDs of the private subnets"
+  value       = module.vpc.private_subnets
+}
+
+output "lb_dns_name" {
+  description = "The DNS name of the Load Balancer"
+  value       = aws_lb.application_lb.dns_name
+}
 
